2a18dac3aea7a7e573053f602206d5f3256800ae
yifei-fu <yifeifu2@gmail.com>
2018-10-05 23:13:23 -0700

    Fix: not serving static files.

--- app.js
+++ app.js
@@ -22,6 +22,7 @@ if (process.env.NODE_ENV !== 'production') {
 }
 
 // serve static files
+app.use(express.static(path.join(__dirname, 'frontend/build')))
 app.get('*', function (req, res) {
     res.sendFile('index.html', { root: path.join(__dirname, 'frontend/build/') })
 })
